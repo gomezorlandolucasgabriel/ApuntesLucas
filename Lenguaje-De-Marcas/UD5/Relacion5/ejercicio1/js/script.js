83b79fbd0ff5f173efffa7fa2ac24f8ba35264f7
gomezorlandolucasgabriel <gomezorlandolucasgabriel001.alu@stemgranada.com>
2026-02-25 20:08:09 +0100

    Guardo relacion 5 de Lenguaje de marcas

---
+++ Lenguaje-De-Marcas/UD5/Relacion5/ejercicio1/js/script.js
@@ -0,0 +1,8 @@
+    const lista = document.getElementById =("lista")
+    const btnAniadir = document.getElementById = ("btnAniadir")
+    const btnBorrar = document.getElementById = ("btnBorrar")
+    const btnEditar = document.getElementById = ("btnEditar")
+
+function aniadir(params) {
+    
+}
